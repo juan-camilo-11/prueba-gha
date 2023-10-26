425cd53be71cc2169951339e7b41024bf8a2d658
jrome50 <jrome50@bancodebogota.com.co>
2023-10-26 11:31:56 -0500

    Agrego otro test

--- src/test/java/com/calculadora/CalcuApplicationTests.java
+++ src/test/java/com/calculadora/CalcuApplicationTests.java
@@ -4,16 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-
 import com.calculadora.servicio.iOperaciones;
-
 
 class CalcuApplicationTests {
 	@Test
-	 public void multiplicacionDosNumeros() {
+	public void multiplicacionDosNumeros() {
 		iOperaciones operaciones = new iOperaciones();
-       int result = operaciones.multiplicacion(3, 6);
-       assertEquals(18, result);
-   }
+		int result = operaciones.multiplicacion(3, 6);
+		assertEquals(18, result);
+	}
+
+	@Test
+	public void sumaDosNumeros() {
+		iOperaciones operaciones = new iOperaciones();
+		int result = operaciones.sumar(3, 6);
+		assertEquals(9, result);
+	}
 
 }
